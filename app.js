32cd4dc12e62fc4fcf42bc78867640113adf8ed1
Ben Barnett <ben@univthink.com>
2019-05-29 19:31:11 -0400

    Smart Alert

--- app.js
+++ app.js
@@ -52,7 +52,7 @@ mongoose.set('useFindAndModify', false);
 mongoose.set('useCreateIndex', true);
 mongoose.set('useNewUrlParser', true);
 mongoose.connect(process.env.MONGODB_URI);
-mongoose.connection.dropDatabase();
+//mongoose.connection.dropDatabase();
 mongoose.connection.on('error', (err) => {
   console.error(err);
   console.log('%s MongoDB connection error. Please make sure MongoDB is running.', chalk.red('✗'));
